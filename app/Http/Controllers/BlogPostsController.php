2f9abc0348aeec8cb02e7779ab5966cde5b053bf
matijavavetic <47482084+matijavavetic@users.noreply.github.com>
2019-07-06 17:51:19 +0200

    rework

--- app/Http/Controllers/BlogPostsController.php
+++ app/Http/Controllers/BlogPostsController.php
@@ -42,16 +42,16 @@ class BlogPostsController extends Controller
         return $this->responseFactory->view('home');
     }
 
-    public function edit($id)
+    public function edit(BlogPosts $blogPosts, $id)
     {
-        $blogPost = BlogPosts::findOrFail($id);
+        $blogPost = $blogPosts->findOrFail($id);
 
         return $this->responseFactory->view('blog.edit', compact('blogPost'));
     }
 
-    public function update(BlogPostRequest $request, $id)
+    public function update(BlogPostRequest $request, BlogPosts $blogPosts, $id)
     {
-        $blogPost = BlogPosts::findOrFail($id);
+        $blogPost = $blogPosts->findOrFail($id);
 
         $blogPost->update($request->validationData());
 
